797a05a6696e6403bea75b9d9d5d7de62988a4aa
Neha Sontakke <wasnik.neha@gmail.com>
2022-05-06 21:48:39 +0530

    added the file

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,4 @@
 // this is your main.js script
 
 alert('Open the browser console whenever you work on JavaScript')
+//1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to  wait for the number of years he neds to turn 18.
