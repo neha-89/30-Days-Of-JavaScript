5a100115e1fe19f155fa3ac94ef1a2067d2e7e9d
Neha Sontakke <wasnik.neha@gmail.com>
2022-05-10 18:23:16 +0530

    added solution-6

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -89,8 +89,27 @@
 //    ```
 
 // 7. Use for loop to iterate from 0 to 100 and print only even numbers
+
+for(let i =0 ;i<= 100;i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
 // 8. Use for loop to iterate from 0 to 100 and print only odd numbers
+for(let i =0 ;i<= 100;i++){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
+
 // 9. Use for loop to iterate from 0 to 100 and print only prime numbers
+for(let i =0 ;i<= 100;i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
 // 10. Use for loop to iterate from 0 to 100 and print the sum of all numbers.
 
 //     ```sh
